32f645abea294479434dfa4ea38f890427b2f8e7
om <om>
2023-01-27 21:24:31 +0100

    console.log added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import HomeComponent from "./components/home";
 
+
+console.log('hello world :D')
+
 function App() {
   return (
     <BrowserRouter>
