fdb5983fb006f85a960ab5dc0dc80f4d6a3a0e7e
DapChri99 <christiandapra08@gmail.com>
2023-02-01 23:34:38 +0300

    backend implemented

---
+++ backend/Login_system/routes/pages.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.render('index')
+})
+
+router.get('/register', (req, res) => {
+    res.render('register')
+})
+
+router.get('/profile', (req, res) => {
+    res.render('profile')
+})
+
+router.get('/home', (req, res) => {
+    res.render('home')
+})
+
+
+module.exports = router
