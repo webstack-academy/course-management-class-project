e4b1c3379722c2b39077dfa5ee5445975b93c259
DapChri99 <christiandapra08@gmail.com>
2023-02-03 23:29:48 +0300

    feat: login implemented

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -4,8 +4,6 @@ const { db } = require('../services/db.js')
 const validator = require('email-validator')
 const crypto = require('crypto') 
 
-
-
 exports.register = (req, res) => {
     console.log(req.body)
  /*
@@ -60,3 +58,30 @@ db.query('select email from users where email =?', [email],
 })
 
 }
+
+
+exports.login = async (req, res) => {
+    const {email, password} = req.body
+
+    if (typeof email!=="string" || validator.validate(email)){
+        return res.render('login', {msg: 'empty email'})
+    }
+
+    if (typeof password !=="string" || password.length <= 0 ){
+        return res.render('login', {msg: 'empty password'})
+    }
+    const result = await db.query('select * from users where email =?', [email])
+    
+    if (!(await bcrypt.compare(password,result[0].password))){
+        return res.status(401).send('login', {msg: 'Email or Password incorrect'})
+    
+    }
+    
+    const token = crypto.randomBytes(16).toString('hex') 
+    
+    db.query('Update users set token =? where id = ? ' , [token, result[0].id])
+    
+    return res.send('login', {msg: 'User Log In Success', token, id: result[0].id})
+
+}
+   
