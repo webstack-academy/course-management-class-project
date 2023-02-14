63ff4de3f62a3bee1297fdb3a6be4995f7a2b4e2
gabrielemarchesano <marchesano.gabriele@gmail.com>
2023-02-14 12:07:51 +0100

    implement: controllers
    development of the controllers(subject to changes)

---
+++ backend/controllers/courses.js
@@ -0,0 +1,11 @@
+exports.createCourse = (req, res) => {
+  const { title, description } = req.body;
+  
+  if (typeof title !== 'string' || title.length < 5 || title.length > 60){
+    return res.status(400).send({ message: 'Title must be a string or between 5 and 60 characters' })
+  }
+
+  if (typeof description !== 'string' || description.length > 300){
+    return res.status(400).send({ message: 'Description must be a string or below 301 characters' })
+  }
+}
